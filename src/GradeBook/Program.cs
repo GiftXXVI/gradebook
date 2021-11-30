98be263e04e530ef5c19535c760119534c941ce8
GiftXXVI <gift.chimphonda@outlook.com>
2021-11-30 15:17:30 +0200

    Added Command Line Argument

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 
-namespace GradeBook{
-    class Program{
-        static void Main(string[] args){
-            Console.WriteLine("Hello, World!");
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            name = (args.Count() > 0) ? args[0] : "Stranger";
+            Console.WriteLine($"Hello, {name}!");
         }
     }
 }
