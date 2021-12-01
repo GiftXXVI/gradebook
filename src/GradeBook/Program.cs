f6f0cc1cdface082a9446b2821f69d665069a369
GiftXXVI <gift.chimphonda@outlook.com>
2021-12-01 07:31:49 +0200

    Added Book Class

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,6 +7,16 @@ namespace GradeBook
         static void Main(string[] args)
         {
             string name = "";
+            Book book = new Book();
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            double result = 0.0;
+            foreach (double grade in grades)
+            {
+                result += grade;
+            }
+            Console.WriteLine($"The sum is {result:N2}");
+            Console.WriteLine($"The average is {result / grades.Count():N2}");
+
             name = (args.Count() > 0) ? args[0] : "Stranger";
             Console.WriteLine($"Hello, {name}!");
         }
