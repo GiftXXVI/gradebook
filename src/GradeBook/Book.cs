48c44bd1a541ab34016cbd7eac0e14fde00ffa22
GiftXXVI <gift.chimphonda@outlook.com>
2021-12-01 11:47:11 +0200

    Added Book Class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,5 +1,22 @@
+using System;
 namespace GradeBook{
     class Book{
-        
+        List<double> grades = new List<double>();
+        public Boolean AddGrades(List<double> in_grades){
+            grades = in_grades;
+            return true;
+        }
+        public Boolean AddGrade(double grade){
+            grades.Add(grade);
+            return true;
+        }
+        public double CalculateAverage(){
+            double result = 0.0;
+            foreach (double grade in grades)
+            {
+                result += grade;
+            }
+            return 0;
+        }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,11 +9,9 @@ namespace GradeBook
             string name = "";
             Book book = new Book();
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            book.AddGrades(grades);
             double result = 0.0;
-            foreach (double grade in grades)
-            {
-                result += grade;
-            }
+            
             Console.WriteLine($"The sum is {result:N2}");
             Console.WriteLine($"The average is {result / grades.Count():N2}");
 
