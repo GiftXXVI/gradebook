3e2cd3a6593f16cdf65406fe54632eedbb9aaa6e
GiftXXVI <gift.chimphonda@outlook.com>
2021-12-02 14:15:16 +0200

    Separated GetGrades method from Main

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,12 +7,38 @@ namespace GradeBook
         static void Main(string[] args)
         {
             Book book = new Book("The Academy");
-            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            List<double> grades = GetGrades();
             book.AddGrades(grades);
             book.ShowStatistics();
             string name = "";
             name = (args.Count() > 0) ? args[0] : "Stranger";
             Console.WriteLine($"Hello, {name}!");
+        }
+
+        static List<double> GetGrades()
+        {
+            List<double> grades = new List<double>();
+            while (true)
+            {
+                Console.WriteLine("Please type a grade or 'q' if finished!");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    if (double.TryParse(input, out double grade) && (grade >= 0) && (grade <= 100))
+                    {
+                        grades.Add(grade);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input Detected!!!");
+                    }
+                }
+            }
+            return grades;
         }
     }
 }
